7ae5b234cad4326772ab80d9b141c63da62b4dda
jaglame <jaglame@gmail.com>
2020-10-16 07:19:03 -0300

    data sumatoria

---
+++ javascript/sumatoria/sumatoria.js
@@ -0,0 +1,61 @@
+let data = [{
+    "id": "3WzFN",
+    "cliente": "1",
+    "nombre": "Vendedor 3",
+    "total": 100,
+    "lista": [{
+            "unidades": "2",
+            "precio": "25",
+            "subtotal": 100,
+            "producto": "Producto 1",
+            "peso": "2"
+            }]
+},{
+    "id": "6Zls",
+    "cliente": "2",
+    "nombre": "Vendedor1",
+    "total": 550,
+    "lista": [{
+            "unidades": "4",
+            "precio": "25",
+            "subtotal": 100,
+            "producto": "Producto 2",
+            "peso": "2"
+         }, {
+            "unidades": "4",
+            "precio": "25",
+            "subtotal": 100,
+            "producto": "Producto 1",
+            "peso": "2"
+         }],
+},{
+    "id": "U2HX",
+    "cliente": "3",
+    "nombre": "Vendedor2",
+    "total": 600,
+    "lista": [{
+            "unidades":"2",
+            "precio":"25",
+            "subtotal":100,
+            "producto":"Producto1",
+            "peso":"2"
+        }, {
+            "unidades": "3",
+            "precio": "25",
+            "subtotal": 100,
+            "producto": "Producto 3",
+            "peso": "2"
+        }]
+}, {
+    "id":"aJixp",
+    "cliente":"4",
+    "nombre": "Vendedor 3",
+    "total": 220,
+    "lista": [{
+            "unidades":"2",
+            "precio":"25",
+            "subtotal":100,
+            "producto":"Producto1",
+            "peso":"2"
+        }]
+}]
